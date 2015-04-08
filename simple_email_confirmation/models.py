e66f9f820cc4ab9d979c23fc5798950f04312f59
Til Boerner <tilman.boerner@gmx.net>
2015-04-08 16:13:18 +0200

    Changes for use with abend.net

--- simple_email_confirmation/models.py
+++ simple_email_confirmation/models.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_init, post_save
+from django.dispatch import receiver
 from django.utils.crypto import get_random_string
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -33,13 +34,23 @@ class SimpleEmailConfirmationUserMixin(object):
     def get_primary_email(self):
         return getattr(self, self.primary_email_field_name)
 
+    def __get_or_create_primary_address(self):
+        """ Returns the EmailAddress matching the user's primary email,
+            creating it if necessary; None if primary email field is blank.
+        """
+        email = getattr(self, self.primary_email_field_name)
+        if email:
+            address, created = self.email_address_set.get_or_create(email=email)
+            return address
+        return None
+
     def set_primary_email(self, email, require_confirmed=True):
         "Set an email address as primary"
         old_email = self.get_primary_email()
         if email == old_email:
             return
 
-        if email not in self.confirmed_emails and require_confirmed:
+        if email not in self.get_confirmed_emails() and require_confirmed:
             raise EmailNotConfirmed()
 
         setattr(self, self.primary_email_field_name, email)
@@ -51,38 +62,28 @@ class SimpleEmailConfirmationUserMixin(object):
     @property
     def is_confirmed(self):
         "Is the User's primary email address confirmed?"
-        return self.get_primary_email() in self.confirmed_emails
+        return self.get_primary_email() in self.get_confirmed_emails()
+
+    @property
+    def has_active_confirmation_request(self):
+        "Is there an active confirmation request for the primary email address?"
+        address = self.__get_or_create_primary_address()
+        return bool(address and address.is_being_confirmed)
 
     @property
     def confirmed_at(self):
         "When the User's primary email address was confirmed, or None"
-        address = self.email_address_set.get(email=self.get_primary_email())
-        return address.confirmed_at
-
-    @property
-    def confirmation_key(self):
-        """
-        Confirmation key for the User's primary email
-
-        DEPRECATED. Use get_confirmation_key() instead.
-        """
-        email = self.get_primary_email()
-        return self.get_confirmation_key(email)
-
-    @property
-    def confirmed_emails(self):
-        "DEPRECATED. Use get_confirmed_emails() instead."
-        return self.get_confirmed_emails()
-
-    @property
-    def unconfirmed_emails(self):
-        "DEPRECATED. Use get_unconfirmed_emails() instead."
-        return self.get_unconfirmed_emails()
+        address = self.__get_or_create_primary_address()
+        return address and address.confirmed_at
 
     def get_confirmation_key(self, email=None):
         "Get the confirmation key for an email"
-        email = email or self.get_primary_email()
-        address = self.email_address_set.get(email=email)
+        if email:
+            address = self.email_address_set.get(email=email)
+        else:
+            address = self.__get_or_create_primary_address()
+            if not address:
+                raise EmailAddress.DoesNotExist('User has no primary email')
         return address.key
 
     def get_confirmed_emails(self):
@@ -92,6 +93,8 @@ class SimpleEmailConfirmationUserMixin(object):
 
     def get_unconfirmed_emails(self):
         "List of emails this User has been associated with but not confirmed"
+        # Make sure primary email is registered:
+        self.__get_or_create_primary_address()
         address_qs = self.email_address_set.filter(confirmed_at__isnull=True)
         return [address.email for address in address_qs]
 
@@ -154,8 +157,8 @@ class EmailAddressManager(models.Manager):
 
     def generate_key(self):
         "Generate a new random key and return it"
-        # sticking with the django defaults
-        return get_random_string()
+        # double django default length for ~142 bits of randomness
+        return get_random_string(length=24)
 
     def create_confirmed(self, email, user=None):
         "Create an email address in the confirmed state"
@@ -217,6 +220,10 @@ class EmailAddress(models.Model):
         blank=True, null=True,
         help_text=_('First time this email was confirmed'),
     )
+    requested_at = models.DateTimeField(
+        blank=True, null=True,
+        help_text=_('Last time confirmation was requested for this email'),
+    )
 
     objects = EmailAddressManager()
 
@@ -224,8 +231,10 @@ class EmailAddress(models.Model):
         unique_together = (('user', 'email'),)
         verbose_name_plural = "email addresses"
 
-    def __unicode__(self):
+    def __str__(self):
         return '{} <{}>'.format(self.user, self.email)
+
+    __unicode__ = __str__
 
     @property
     def is_confirmed(self):
@@ -234,6 +243,10 @@ class EmailAddress(models.Model):
     @property
     def is_primary(self):
         return bool(self.user.email == self.email)
+
+    @property
+    def is_being_confirmed(self):
+        return self.requested_at is not None and not self.is_key_expired
 
     @property
     def key_expires_at(self):
@@ -251,15 +264,23 @@ class EmailAddress(models.Model):
     def reset_confirmation(self):
         """
         Re-generate the confirmation key and key expiration associated
-        with this email.  Note that the previou confirmation key will
+        with this email.  Note that the previous confirmation key will
         cease to work.
         """
         self.key = self._default_manager.generate_key()
         self.set_at = timezone.now()
 
         self.confirmed_at = None
-        self.save(update_fields=['key', 'set_at', 'confirmed_at'])
+        self.requested_at = None
+        self.save(update_fields=['key', 'set_at', 'confirmed_at', 'requested_at'])
         return self.key
+
+    def set_requested(self, when=None):
+        "Records that a confirmation request has been made"
+        if when is None:
+            when = timezone.now()
+        self.requested_at = when
+        self.save(update_fields=['requested_at'])
 
 
 # by default, auto-add unconfirmed EmailAddress objects for new Users
@@ -274,12 +295,20 @@ if getattr(settings, 'SIMPLE_EMAIL_CONFIRMATION_AUTO_ADD', True):
                 email = user.get_primary_email()
             else:
                 email = user.email
-            if hasattr(user, 'add_unconfirmed_email'):
-                user.add_unconfirmed_email(email)
-            else:
-                user.email_address_set.create_unconfirmed(email)
+            if email:
+                if hasattr(user, 'add_unconfirmed_email'):
+                    user.add_unconfirmed_email(email)
+                else:
+                    user.email_address_set.create_unconfirmed(email)
 
     # TODO: try to only connect this to the User model. We can't use
     #       get_user_model() here - results in import loop.
 
     post_save.connect(auto_add)
+
+
+@receiver(post_init, sender=EmailAddress)
+def _auto_generate_confirmation_key(sender, instance, **kwargs):
+    "Make sure EmailAddresses have a confirmation key after creation"
+    if not instance.key:
+        instance.key = instance._default_manager.generate_key()
